cef2c6de3edb37289c33fecb3fbad519171911ca
Dusty McLean <dusty_mclean@icloud.com>
2026-03-30 11:40:49 -0500

    Revert "Add custom descriptions, color variants, and image galleries"
    This reverts commit 326d356f94024e47284ccac7664e579f142f84a5.

--- test_grid2.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-html = requests.get('https://thermodynesystems.com/wholesaler/us/vapes.html').text
-soup = BeautifulSoup(html, 'html.parser')
-p = soup.select_one('li.item.product')
-if p:
-    print(p.prettify())
